edc545a9dbc7e6a5f31d0e0bfc24bc5b0018d233
Jeremiah Burrus <burrujer000@warren.k12.in.us>
2024-11-08 10:01:44 -0500

    initial committ

--- number.py
+++ number.py
@@ -1,13 +1,11 @@
 import random
-n = random.randrange(1,10)
-guess = int(input("Enter any number:"))
-while n!= guess:
+n = random.randint(1, 10)
+while True:
+    guess = int(input("Enter any number:"))
     if guess < n:
         print("Too Low")
-        guess = int(input("Guess the number again:"))
     elif guess > n:
         print("Too high!")
-        guess = int(input("Enter number again:"))
     else:
-     break
-print("you guessed it right !!")
+        print("you guessed it right !!")
+        break
